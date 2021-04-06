e4eeeebdc3a4e2ad3c049f535f2d835a711c75f1
Romny468 <51722143+Romny468@users.noreply.github.com>
2021-04-06 15:22:02 +0200

    Update password_checker_v2.py

--- password_checker/password_checker_v2.py
+++ password_checker/password_checker_v2.py
@@ -1,15 +1,5 @@
-import string
-
-# check password security
-def passwdCheck(passwd):
-    passworderror = "Your password is missing the following:"
-    if len(passwd) < 8: passworderror = passworderror + "\n + at least 8 characters"
-    if not any(char.islower() for char in passwd): passworderror = passworderror + "\n + at least one lower case character"
-    if not any(char.isupper() for char in passwd): passworderror = passworderror + "\n + at least one upper case character"
-    if not any(char.isdigit() for char in passwd): passworderror = passworderror + "\n + at least one digit"
-    if not any(char in string.punctuation for char in passwd): passworderror = passworderror + "\n + at least one special character"
-    if passworderror == "Your password is missing the following:": return True
-    else: print("\n", passworderror,sep="")
+# import defenition from other file
+from passwdCheck_v1 import passwdCheck
 
 # program beginning
 passwd = input("Give me your password and I will check it for toughness: ")
